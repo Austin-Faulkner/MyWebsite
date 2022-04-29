73485705b3240a9ce392897217e80d5b411deef5
Austin-Faulkner <faulkner.austin@gmail.com>
2022-04-29 04:13:40 -0500

    first commit 042922 4:13 am

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():  # put application's code here
+    return render_template('homepage02.html')
+
+
+@app.route('/work_exp')
+def work_exp():
+    return render_template('work_exp.html')
+
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+if __name__ == '__main__':
+    app.run()
+
